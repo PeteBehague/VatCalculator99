af3ab74ef6ed275036305fd449cea84c5a5fd4a4
PeteBehague <Peter.Behague@qa.com>
2022-10-11 09:09:32 +0100

    Finished App

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import DisplayBlock from './DisplayBlock';
+import PriceEntryField from './PriceEntryField';
+import VatRateField from './VatRateField';
 
 function App() {
+  const [netPrice, setNetPrice] = useState(0.0);
+  const [grossPrice, setGrossPrice] = useState(0.0);
+  const [vatRate, setVatRate] = useState(20.0);
+  const [vatToPay, setVatToPay] = useState(0.0);
+
+  const handleNetPriceChange = (price) => {
+    const gross_price = price * ((vatRate / 100) + 1);
+    setNetPrice(price);
+    setGrossPrice(gross_price);
+    setVatToPay(gross_price - price);
+  }
+  
+  const handleGrossPriceChange = (price) => {
+    const net_price = price / ((vatRate / 100) + 1);
+    setNetPrice(net_price);
+    setGrossPrice(price);
+    setVatToPay(price - net_price);
+  }
+
+  const handleVatRateChanged = (rate) => {
+      setVatRate(rate)
+      updatePrices()
+  }
+
+  const updatePrices = () => {
+    handleNetPriceChange(netPrice)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header field">
+        VAT Calculator
+        <div className='pale-green-border'>
+          <VatRateField customstyle="field" vatRateChanged={handleVatRateChanged} value={vatRate} updatePrices={updatePrices}/>
+          <PriceEntryField label="Price excl VAT: " priceChanged={handleNetPriceChange} price={netPrice === 0.0 ? "": netPrice} customstyle="field" />
+          <DisplayBlock label="Vat to pay: " value={vatToPay} customstyle="field" />
+          <PriceEntryField label="Price incl VAT: " priceChanged={handleGrossPriceChange} price={grossPrice === 0.0 ? "": grossPrice} customstyle="field" />  
+        </div>      
     </div>
   );
 }
